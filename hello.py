e9a169a7f9044a7560f748ee05dc4ff297b30097
IssareeW <125816888+IssareeW@users.noreply.github.com>
2023-02-19 23:50:20 +0700

    update Hello

--- hello.py
+++ hello.py
@@ -1,3 +1,8 @@
 print ('Belle')
+print ('Hello, I love you')
 print ('Loves')
+print ('Hello, you love me')
 print ('Gift')
+print ('Hello, update Github')
+friends=['Bank','Belle','Boss']
+print(friend[0])
